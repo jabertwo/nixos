667f7770b8a82c1b0749db98b00806b1253bd690
jabertwo <git@jabertwo.de>
2025-12-11 00:24:39 +0100

    install codium

--- configuration.nix
+++ configuration.nix
@@ -154,6 +154,20 @@ in
         user.Email = "git@jabertwo.de";
       };
     };
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix         # Nix language support
+        ];
+        userSettings = {
+          "editor.fontSize" = 14;
+          "terminal.integrated.defaultProfile.linux" = "bash";
+        };
+      };
+    };
     dconf = {
       enable = true;
       settings = {
