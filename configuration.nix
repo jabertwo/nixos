1cd92fe0196624b94fa7213aaf5d7a2de9a98c50
jabertwo <git@jabertwo.de>
2025-12-11 19:56:29 +0100

    move user config to seperate file

--- configuration.nix
+++ configuration.nix
@@ -95,13 +95,6 @@ in
     #media-session.enable = true;
   };
 
-   #Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.jabertwo = {
-    isNormalUser = true;
-    description = "jabertwo";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-
   # Enable fingerint support
   services.fprintd.enable = true;
   
@@ -139,57 +132,6 @@ in
 
   # home manager
   home-manager.useGlobalPkgs = true;
-  home-manager.users.jabertwo = {
-    home.stateVersion = "25.11";
-    home.packages = [
-      pkgs.gnomeExtensions.ddterm
-      pkgs.gnomeExtensions.dash-to-dock
-      pkgs.element-desktop
-      pkgs.vscodium
-    ];
-    programs.git = {
-      enable = true;
-      settings = {
-        user.Name = "jabertwo";
-        user.Email = "git@jabertwo.de";
-      };
-    };
-    programs.vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-
-      profiles.default = {
-        extensions = with pkgs.vscode-extensions; [
-          bbenoist.nix         # Nix language support
-        ];
-        userSettings = {
-          "editor.fontSize" = 14;
-          "terminal.integrated.defaultProfile.linux" = "bash";
-        };
-      };
-    };
-    programs.element-desktop = {
-      enable = true;
-    };
-    dconf = {
-      enable = true;
-      settings = {
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = with pkgs.gnomeExtensions; [
-            ddterm.extensionUuid
-            dash-to-dock.extensionUuid
-          ];
-        };
-        "org/gnome/desktop/wm/preferences" = {
-          "button-layout" = ":minimize,maximize,close";
-        };
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-        };
-      };
-    };
-  };
 
   # remove unnecesary gnome packages
   environment.gnome.excludePackages = (with pkgs; [
