124c554d823d449ba28cf18de6cb5f15b68adeec
jabertwo <git@jabertwo.de>
2025-12-16 14:21:20 +0100

    seperate config file for gnome

---
+++ utils/gnome.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.gnome.gcr-ssh-agent.enable = false;
+ 
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+
+  # remove unnecesary gnome packages
+  environment.gnome.excludePackages = (with pkgs; [
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gnome-characters
+    gnome-music
+    gnome-photos
+    gnome-tour
+    totem
+  ]);
+}
