60b51614528a087e337f4d17a5125b5eca36919a
jabertwo <git@jabertwo.de>
2025-12-31 19:17:53 +0100

    enable cachix for devenv

--- utils/base.nix
+++ utils/base.nix
@@ -34,7 +34,15 @@
     ansible
     ansible-lint
     mtr
+    htop
+    devenv
   ];
+
+  # Enable caching for devenv
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
