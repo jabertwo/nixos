ac429678a6389edfc035aab8bfcdb337ba359135
jabertwo <git@jabertwo.de>
2026-05-15 20:49:39 +0200

    update ssh keys

--- users/jberges-rpi4.nix
+++ users/jberges-rpi4.nix
@@ -11,8 +11,9 @@
     shell = pkgs.zsh;
     initialHashedPassword = "$y$j9T$ULBqWjxo/xCsHsvRT2Q1C0$ss5IiThBxWa.YYk4qMAu/pEyq.DT4DY27phKC6LmGc7";
     openssh.authorizedKeys.keys = [
+      "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBMU/DG+S/3fGsXsQk6cTOClLH8LXFtL9IL8u6B6Pr1xC4iluZ2xoqQvsYIx5H2sX3nw6WM/VoEVP+xMxEazoOKAwB/31OazpYGG3JGuDvOVlbRVHNoxF9wn3JY9uPyI+Jg== jabertwo-home"
       "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBGR9N60F+0annoCi9cM+94jSxsw8KPgMf7GqKoFmxwpcDf6fd7Vc5sRQg0avnEg009D2nxihED0y2eTP2Tzn6eQQ/2LRXRfMCa+hRK99YYPUjpszH/y2bC2r/08CvcdeVA== jabertwo-mob"
-    ];
+    ]
   };
 
   security.sudo.extraRules = [
