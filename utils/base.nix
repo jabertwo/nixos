d20ae4ee67e15d9eed95f99bb471780b7c9b5f15
jabertwo <git@jabertwo.de>
2026-08-11 16:50:37 +0200

    enable flatpak

--- utils/base.nix
+++ utils/base.nix
@@ -56,6 +56,9 @@
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
+  # Enable flatpak
+  services.flatpak.enable = true;
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
